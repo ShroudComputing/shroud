0e8de304101cfdd59ab572ea426b740c046e4e1a
PostCrafter <dev@postcrafter.de>
2016-01-30 23:36:51 +0100

    Global variable for shrouds root path

--- app/index.js
+++ app/index.js
@@ -1,10 +1,17 @@
+var path = require('path');
+
+global.__shroud = path.join(__dirname, 'bin') + path.sep;
+
+var Registry = require(__shroud + 'registry.js');
+
 var internals = {};
 
-internals.App = function () {
+internals.App = function() {
 
+  this.components = new Registry();
 };
 
-internals.App.prototype.init = function () {
+internals.App.prototype.init = function() {
 
 };
 
