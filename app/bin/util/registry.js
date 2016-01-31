218c460644a0dc0889f946871b8c94a47ef2a924
PostCrafter <dev@postcrafter.de>
2016-01-31 02:30:23 +0100

    Add component aliases

--- app/bin/util/registry.js
+++ app/bin/util/registry.js
@@ -4,14 +4,18 @@ internals.Registry = function () {
   this.clear();
 };
 
-internals.Registry.prototype.add = function(name, dependencies, reference) {
+internals.Registry.prototype.add = function(name, dependencies, reference, aliases) {
   if (this.components[name] !== undefined) {
     throw new Error('Component ' + name + ' is already registered. Use swap() instead.');
+  }
+  if (this.aliases[name] !== undefined) {
+    throw new Error('Alias ' + name + ' is already registered.');
   }
   var component = {
     name: name,
     depends: dependencies,
-    instance: null
+    instance: null,
+    aliases: aliases
   };
   if (typeof component === 'function') {
     if (reference.length !== dependencies.length) {
@@ -25,12 +29,18 @@ internals.Registry.prototype.add = function(name, dependencies, reference) {
     component.instance = reference;
   }
   this.components[name] = component;
+  if (typeof aliases !== 'undefined') {
+    this.alias(name, aliases);
+  }
 };
 
-internals.Registry.prototype.get = function(componentName) {
-  var component = this.components[componentName];
+internals.Registry.prototype.get = function(name) {
+  if (this.aliases[name]) {
+    name = this.aliases[name];
+  }
+  var component = this.components[name];
   if (typeof component === 'undefined') {
-    throw new Error('Component ' + componentName + ' not found');
+    throw new Error('Component ' + name + ' not found');
   }
   if (!component.instance) {
     var dependencies = this.getDependencies(component);
@@ -40,6 +50,10 @@ internals.Registry.prototype.get = function(componentName) {
 };
 
 internals.Registry.prototype.remove = function(name) {
+  var self = this;
+  if (this.aliases[name]) {
+    name = this.aliases[name];
+  }
   var component = this.components[name];
   if (typeof component === 'undefined') {
     return;
@@ -48,15 +62,19 @@ internals.Registry.prototype.remove = function(name) {
     throw new Error('Component ' + name + ' was already instantiated');
   }
   delete this.components[name];
+  component.aliases.forEach(function(alias) {
+    delete self.aliases[alias];
+  });
 };
 
-internals.Registry.prototype.swap = function(componentName, dependencies, reference) {
-  this.remove(componentName);
-  this.add(componentName, dependencies, reference);
+internals.Registry.prototype.swap = function(name, dependencies, reference) {
+  this.remove(name);
+  this.add(name, dependencies, reference);
 };
 
 internals.Registry.prototype.clear = function() {
   this.components = {};
+  this.aliases = {};
 };
 
 internals.Registry.prototype.getDependencies = function(component) {
@@ -67,6 +85,19 @@ internals.Registry.prototype.getDependencies = function(component) {
       throw new Error('Component ' + dependency + ' can\'t depend on itself');
     }
     res.push(self.get(dependency));
+  });
+};
+
+internals.Registry.prototype.alias = function(name, aliases) {
+  var self = this;
+  if (typeof aliases === 'string') {
+    aliases = [aliases];
+  }
+  aliases.forEach(function(alias) {
+    if (self.components[alias] !== undefined) {
+      throw new Error('Component ' + alias + ' is already registered. can\'t create alias for this name.');
+    }
+    self.aliases[alias] = name;
   });
 };
 
