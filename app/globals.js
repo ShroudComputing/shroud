d84ccd3a275d7da0d5e4f92fad9984c178b795d1
PostCrafter <dev@postcrafter.de>
2016-02-09 18:43:52 +0100

    Resolve shroud path by dir

--- app/globals.js
+++ app/globals.js
@@ -4,7 +4,10 @@ const internals = {};
 
 internals.path = {};
 internals.path.base = __dirname;
-internals.path.bin = path.join(internals.path.base, 'bin');
+internals.path.resolve = function(dir) {
+  return path.join(internals.path.base, dir);
+};
+internals.path.bin = internals.path.resolve('bin');
 
 internals.require = function(requirePath) {
   return path.join(internals.path.bin, requirePath);
