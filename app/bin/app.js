57643f30406bfac84b761075ef0925a77aab9596
PostCrafter <dev@postcrafter.de>
2016-02-09 18:46:23 +0100

    App is a container

--- app/bin/app.js
+++ app/bin/app.js
@@ -2,12 +2,16 @@ const util = require('util');
 
 const EventEmitter = require('events').EventEmitter;
 
+const Container = require(__shroud.require('util/container.js'));
+
 const internals = {};
 
 internals.App = function() {
   EventEmitter.call(this);
+  Container.call(this);
 };
 
 util.inherits(internals.App, EventEmitter);
+util.inherits(internals.App, Container);
 
 module.exports = exports = internals.App;
