a799368fd47a5e7baf47e6b344b111a7d885402d
PostCrafter <dev@postcrafter.de>
2016-02-10 17:35:06 +0100

    Shroud is a singleton of the shroud app

--- app/index.js
+++ app/index.js
@@ -3,25 +3,5 @@ const util = require('util');
 global.__shroud = require('./globals.js');
 
 const App = require(__shroud.require('app.js'));
-const Container = require(__shroud.require('util/container.js'));
 
-const internals = {};
-
-internals.Shroud = function() {
-  Container.call(this);
-};
-
-util.inherits(internals.Shroud, Container);
-
-internals.Shroud.prototype.init = function() {
-  this.app();
-};
-
-internals.Shroud.prototype.app = function() {
-  if (!this.hasData('app')) {
-    this.setData('app', new App());
-  }
-  return this.getData('app');
-};
-
-module.exports = exports = new internals.Shroud();
+module.exports = exports = new App();
