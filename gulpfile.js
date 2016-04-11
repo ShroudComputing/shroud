d905e1d9375cc8096e05f73e612e0162f3a3e9dd
PostCrafter <dev@postcrafter.de>
2016-04-12 00:55:48 +0200

    Improve todo file generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const url = require('url');
+const util = require('util');
+
+const pkg = require('./package.json');
+
 const gulp = require('gulp');
 
 const eslint = require('gulp-eslint');
@@ -9,7 +14,7 @@ const todo = require('gulp-todo');
 const source = ['**/*.js','!node_modules/**'];
 
 gulp.task('dev', ['lint', 'doc', 'server:start'], function() {
-  gulp.watch(source, ['lint', 'server:restart']);
+  gulp.watch(source, ['lint', 'doc', 'server:restart']);
 });
 
 gulp.task('server:start', function() {
@@ -29,9 +34,24 @@ gulp.task('lint', function () {
     .pipe(eslint.failAfterError());
 });
 
+
 gulp.task('todo', function() {
   gulp.src(source)
-    .pipe(todo())
+    .pipe(todo({
+      transformComment: function(file, line, text) {
+        let columns = [];
+        const urlpath = url.resolve(pkg.homepage, file).toString();
+        const filepath = urlpath.substring(pkg.homepage.length);
+
+        columns.push(null);
+        columns.push(util.format('[%s](%s)', filepath, urlpath));
+        columns.push(util.format('[%s](%s#%s)', line, urlpath, line));
+        if (text) {
+          columns.push(text);
+        }
+        return columns.join('|');
+      }
+    }))
     .pipe(gulp.dest('./doc'));
 });
 
